138511a405637cc3a41ead7115f727ea014a497e
Elliott Hughes <elliott.huge@gmail.com>
2021-10-03 17:47:21 +0100

    Refactored system class, avoided a bug in code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TickDetector
 PTN's own in-house BGS tick detector for Elite Dangerous.
+
+## How it works
+A System object is maintained for every (populated) system pinged on EDDN within the last hour, on 5 minute intervals.
+
+To save memory, a hash of system's overall faction state at each interval is created and stored on receipt of an faction status update
--- system.py
+++ system.py
@@ -1,15 +1,22 @@
 class System:
-    def __init__(self, name: str):
+    def __init__(self, name: str, hashVal: int):
+        # unique identifier
         self.name = name
-        self.hashedStates = [None]*12
+
+
+        self.hashedStates = [None]*11
+        self.hashedStates.append(hashVal)
+
+
         self.ticked = None
         self.intervalsSinceTick = 0
+        self.intervalsSinceUpdate = 0
 
-
-    # Public Functions
+        self.deletionMark = False
 
     # Called every 5 minutes
     def interval(self):
+        """Performs the system's status management, designed to work on 5 minute intervals."""
         self.hashedStates = self.hashedStates[1:11]
         self.hashedStates.append(None)
 
@@ -19,13 +26,23 @@ class System:
             # Unticks system after 1hr
             if self.intervalsSinceTick >= 12:
                 self.ticked = False
+        
+        self.intervalsSinceUpdate += 1
+        if self.intervalsSinceUpdate == 5:
+            self.deletionMark = True
 
 
     def receiveStateUpdate(self, hashVal: int):
+        # if updateState is already most current
         if self.hashedStates[11] == hashVal:
             return
         
-        # Only reached if system ticks
+        # State is already present but not most current
+        if hashVal in self.hashedStates:
+            self.hashedStates[11] = hashVal
+            return
+        
+        # State is entirely new
         self.hashedStates[11] = hashVal
         self.ticked = True
         self.intervalsSinceTick = 0
