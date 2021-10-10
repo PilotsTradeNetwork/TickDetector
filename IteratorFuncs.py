3a941abb117870e88a2a8d3ad1e39f92e6eaba5f
Elliott Hughes <elliott.huge@gmail.com>
2021-10-10 17:30:17 +0100

    integrated basic discord webhook functionality, stored webhook url in a private file

--- IteratorFuncs.py
+++ IteratorFuncs.py
@@ -2,18 +2,23 @@ from datetime import datetime
 from threading import Thread
 import time
 from system import systemList
+from privateInfo import TEST_DISCORDWEBHOOKURL
+from discord_webhook import DiscordWebhook
 
 
 
 class iteratorThread(Thread):
+    global systemList
 
     def __init__(self, intervalMins: int = 5):
         super().__init__()
         self.intrvlMins = intervalMins * 60
+        
+        self.tracked = 0
+        self.observed = 0
+        self.ticked = 0
 
     def run(self):
-        global systemList
-
         while True:
             time.sleep(self.intrvlMins)
             #   iterate over system list
@@ -22,24 +27,30 @@ class iteratorThread(Thread):
             print("\nIteration beginning")
             systemList[:] = [sys for sys in systemList if not sys.performInterval()]
             self.__printTracking(systemList)
+            self.__sendStatusToDiscord()
 
             # do thing (send the info somewhere useful, like push to discord or do a webhook thing or update a website)
 
     def __printTracking(self, systemList):
-        tracked = 0
-        observed = 0
-        ticked = 0
+        self.tracked = 0
+        self.observed = 0
+        self.ticked = 0
 
         for sys in systemList:
             if sys.isTicked == True:
-                ticked += 1
-                tracked += 1
-                observed += 1
+                self.ticked += 1
+                self.tracked += 1
+                self.observed += 1
             elif sys.isTicked == False:
-                tracked += 1
-                observed += 1
+                self.tracked += 1
+                self.observed += 1
             elif sys.isTicked == None:
-                observed += 1
+                self.observed += 1
         now = datetime.now()
         nowFormatted = now.strftime("%H:%M:%S")
-        print(f"Time: {nowFormatted}\nTicked Systems in last Hr = {ticked},\nCurrently Tracked Systems = {tracked},\nCurrently Observed Systems = {observed}")
+        print(f"Time: {nowFormatted}\nTicked Systems in last Hr = {self.ticked},\nCurrently Tracked Systems = {self.tracked},\nCurrently Observed Systems = {self.observed}")
+
+    def __sendStatusToDiscord(self):
+        webhook = DiscordWebhook(url=TEST_DISCORDWEBHOOKURL, content=f'Tracked Systems: {self.tracked}\nTicked Systems: {self.ticked}')
+        response = webhook.execute()
+
