b86cf81b5b47c5dcd94d7c4684f9b52a0208fdf7
Elliott Hughes <elliott.huge@gmail.com>
2021-10-10 21:33:08 +0100

    removed proof of concept code in main file

--- main.py
+++ main.py
@@ -5,22 +5,10 @@ from IteratorFuncs import iteratorThread
 
 
 def main():
-    # # 2 threads
-    # a = ['a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b']
-    # a.append('c')
-    # a.pop(0)
-    # # a[:] = [x for x in a if x == 'a']
-    # print(a)
-    # input()
-
-
     iteratorThread().start()
     print("IteratorThread started")
     EDDNThread().start()
     print("EDDNThread started")
-
-
-
 
 if __name__ == '__main__':
     main()
